d735080880596ee58856023c9273e71243ef454c
ManonVeselovsky <manonc.v@gmail.com>
2023-01-24 15:44:16 -0500

    Initial commit

---
+++ Github_setup.R
@@ -0,0 +1,9 @@
+usethis::git_sitrep()
+usethis::use_git_config(
+  user.name = "ManonVeselovsky",
+  user.email = "manonc.v@gmail.com"
+)
+usethis::create_github_token()
+credentials::set_github_pat()
+usethis::use_git()
+usethis::git_vaccinate()
